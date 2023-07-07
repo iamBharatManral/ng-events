ce27fc0bcda71b3ed71190c94ca25ae474be458d
iamBharatManral <justbharatmanral@gmail.com>
2023-07-07 13:58:03 +0530

    feat: add notification when saving new session, event, and profile

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -2,31 +2,34 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../auth.service";
 import {Router} from "@angular/router";
+import {NotificationService} from "../../notification.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: 'app-profile', templateUrl: './profile.component.html', styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit{
-  profileForm: FormGroup|any;
-  constructor(private authService: AuthService, private router: Router) {
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup | any;
+
+  constructor(private authService: AuthService, private router: Router, private notification: NotificationService) {
   }
+
   ngOnInit() {
     let firstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern("^[a-zA-Z].*")])
     let lastName = new FormControl(this.authService.currentUser.lastName, [Validators.required, Validators.pattern("^[a-zA-Z].*")])
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: firstName, lastName: lastName
     })
   }
-  cancel(){
+
+  cancel() {
     this.router.navigate(["/events"])
   }
-  saveProfile(formValues:{firstName: string, lastName: string}){
-    if(this.profileForm.valid){
-    this.authService.saveUpdatedProfile(formValues.firstName, formValues.lastName)
-    this.router.navigate(["/events"])
+
+  saveProfile(formValues: { firstName: string, lastName: string }) {
+    if (this.profileForm.valid) {
+      this.authService.saveUpdatedProfile(formValues.firstName, formValues.lastName)
+      this.notification.success()
+      this.router.navigate(["/events"])
     }
   }
 }
